ce123b3f0c8e570dd2f14d2f449626ce7e735bf7
Abril <abrudomingueznoya@gmail.com>
2025-09-09 13:51:48 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Condicion
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número");
+            string linea = Console.ReadLine();
+            int numero = int.Parse(linea);
+
+            if (numero > 0)
+            {
+                Console.WriteLine("El número es positivo");
+            }
+            else
+            {
+                if (numero < 0)
+                {
+                    Console.WriteLine("El número es negativo");
+                }
+                else
+                {
+                    Console.WriteLine("El número es cero");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
